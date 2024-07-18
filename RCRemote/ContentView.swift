1c50e41c7d44144ca534f25611d8dda088ea0725
Qiandao Liu <mlwrf9734@163.com>
2024-07-18 16:26:51 -0400

    Initial Commit

---
+++ RCRemote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RCRemote
+//
+//  Created by Qiandao Liu on 7/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RCRemote/RCRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RCRemoteApp.swift
+//  RCRemote
+//
+//  Created by Qiandao Liu on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct RCRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
